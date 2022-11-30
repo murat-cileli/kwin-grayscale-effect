e70a4645cdd354e5dd1b08a7f964c073865b4390
Aviana Cruz <gwencroft@proton.me>
2022-11-30 18:01:57 +0800

    Adapt to KWin 5
    Signed-off-by: Aviana Cruz <gwencroft@proton.me>

--- README.md
+++ README.md
@@ -2,26 +2,25 @@
 
 Configurable grayscale effect for KWin window manager and KDE desktop environment.
 
-![screenshot_20180917_191805](https://user-images.githubusercontent.com/6532000/50812558-3a565e80-1324-11e9-9006-19422928b3fb.png)
-
-![screenshot_20180917_090716](https://user-images.githubusercontent.com/6532000/50812559-3cb8b880-1324-11e9-8b06-0bd04ae763bd.png)
-
 # Installation
 
-1. [Download](https://github.com/murat-cileli/kwin-grayscale-effect/archive/master.zip) or Git Pull
-2. Copy "kwin4_effect_grayscale" folder to "/usr/share/kwin/effects", using e.g.
+1. [Download](https://github.com/skbeh/kwin-grayscale-effect/archive/master.zip) or Git Pull
+2. Copy "kwin4_effect_grayscale" folder to "/usr/share/kwin/effects/", using e.g.
+
 ```{.bash}
-sudo cp -r kwin4_effect_grayscale /usr/share/kwin/effects
+sudo cp -r kwin4_effect_grayscale /usr/share/kwin/effects/
 ```
-3. Copy "kwin4_effect_grayscale.desktop" to "/usr/share/kservices5/kwin", using e.g.
+
+3. Copy "kwin4_effect_grayscale/metadata.json" to "/usr/share/kservices5/kwin/", using e.g.
+
 ```{.bash}
-sudo cp kwin4_effect_grayscale.desktop /usr/share/kservices5/kwin
+sudo cp kwin4_effect_grayscale/metadata.json /usr/share/kservices5/kwin/
 ```
+
 4. Add "kwin4_effect_grayscaleEnabled=true" to "[Plugins]" section in "~/.config/kwinrc" (as a normal user).
-In case the [Plugins] section is missing from the kwinrc file, just add it.
+   In case the [Plugins] section is missing from the kwinrc file, just add it.
 
 To test it for the first time, open `System Settings` > `Desktop Behavior`. In there, open `Desktop Effects`. You should see `Grayscale` in the `Appearance` section. Toggle it a couple of times, and you'll see the grayscale effect at work.
-
 
 ### Known Quirks
 
--- kwin4_effect_grayscale/contents/code/main.js
+++ kwin4_effect_grayscale/contents/code/main.js
@@ -1,4 +1,4 @@
-/********************************************************************
+/** ******************************************************************
  KWin - the KDE window manager
  This file is part of the KDE project.
 
@@ -16,80 +16,82 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*********************************************************************/
-/*global effect, effects, animate, cancel, set, animationTime, Effect, QEasingCurve */
-/*jslint continue: true */
-var grayscaleEffect = {
-    loadConfig: function() {
-        "use strict";
-        grayscaleEffect.isApplyInactiveWindowsOnly = effect.readConfig("ApplyInactiveWindowsOnly", false);
-        grayscaleEffect.isExcludePanels = effect.readConfig("ExcludePanels", false);
-        grayscaleEffect.effectStrength = effect.readConfig("EffectStrength", 100);
-    },
-    startAnimation: function (window, duration) {
-        "use strict";
+******************************************************************** */
+/* global effect, effects, animate, cancel, set, animationTime, Effect, QEasingCurve */
 
-        // Do not apply invisible windows
-        if (window.visible === false) {
-            return false;
-        }
+'use strict';
 
-        // Do not apply active window by config
-        if (grayscaleEffect.isApplyInactiveWindowsOnly === true && window == effects.activeWindow) {
-            return false;
-        }
+const grayscaleEffect = {
+  loadConfig() {
+    grayscaleEffect.isApplyInactiveWindowsOnly = effect.readConfig(
+      'ApplyInactiveWindowsOnly',
+      false
+    );
+    grayscaleEffect.isExcludePanels = effect.readConfig('ExcludePanels', false);
+    grayscaleEffect.effectStrength = effect.readConfig('EffectStrength', 100);
+  },
+  startAnimation(window) {
+    // Do not apply invisible windows
+    if (!window.visible) return;
 
-        // Do not apply panels by config
-        if (grayscaleEffect.isExcludePanels === true && window.windowClass.indexOf('plasmashell') !== -1) {
-            return false;
-        }
-
-        window.animation = set({
-            window: window,
-            duration: animationTime(100),
-            animations: [{
-                type: Effect.Saturation,
-                to: 1.0 - parseFloat(grayscaleEffect.effectStrength / 100)
-            }]
-        });
-    },
-    cancelAnimation: function (window) {
-        "use strict";
-        if (window.animation !== undefined) {
-            cancel(window.animation);
-            window.animation = undefined;
-        }
-    },
-    desktopChanged: function () {
-        "use strict";
-        var i, windows, window;
-        
-        windows = effects.stackingOrder;
-        
-        for (i = 0; i < windows.length; i++) {
-            grayscaleEffect.cancelAnimation(windows[i]);
-            grayscaleEffect.startAnimation(windows[i]);
-        }
-    },
-    init: function () {
-        "use strict";
-        var i, windows;
-
-        effects.windowClosed.connect(grayscaleEffect.cancelAnimation);
-        effects.windowMinimized.connect(grayscaleEffect.cancelAnimation);
-        effects.windowUnminimized.connect(grayscaleEffect.startAnimation);
-        effects['desktopChanged(int,int)'].connect(grayscaleEffect.desktopChanged);
-        effects.desktopPresenceChanged.connect(grayscaleEffect.cancelAnimation);
-        effects.desktopPresenceChanged.connect(grayscaleEffect.startAnimation);
-
-        grayscaleEffect.loadConfig();
-
-        windows = effects.stackingOrder;
-
-        for (i = 0; i < windows.length; i++) {
-            grayscaleEffect.startAnimation(windows[i]);
-        }
+    // Do not apply active window by config
+    if (
+      grayscaleEffect.isApplyInactiveWindowsOnly === true &&
+      window === effects.activeWindow
+    ) {
+      return;
     }
+
+    // Do not apply panels by config
+    if (
+      grayscaleEffect.isExcludePanels === true &&
+      window.windowClass.indexOf('plasmashell') !== -1
+    ) {
+      return;
+    }
+
+    window.animation = set({
+      window,
+      duration: animationTime(100),
+      animations: [
+        {
+          type: Effect.Saturation,
+          to: 1 - grayscaleEffect.effectStrength / 100,
+        },
+      ],
+    });
+  },
+  cancelAnimation(window) {
+    if (window.animation) {
+      cancel(window.animation);
+      window.animation = undefined;
+    }
+  },
+  restartAnimation(window) {
+    grayscaleEffect.cancelAnimation(window);
+    grayscaleEffect.startAnimation(window);
+  },
+  windowActivated(activatedWindow) {
+    if (!activatedWindow) return;
+    if (grayscaleEffect.isApplyInactiveWindowsOnly)
+      grayscaleEffect.cancelAnimation(activatedWindow);
+    effects.stackingOrder
+      .filter((element) => !element.animation)
+      .forEach(grayscaleEffect.startAnimation);
+  },
+  desktopChanged() {
+    effects.stackingOrder.forEach(grayscaleEffect.restartAnimation);
+  },
+  init() {
+    effects.windowActivated.connect(grayscaleEffect.windowActivated);
+    effects['desktopChanged(int,int)'].connect(grayscaleEffect.desktopChanged);
+    effects.desktopPresenceChanged.connect(grayscaleEffect.restartAnimation);
+    effects.windowClosed.connect(grayscaleEffect.cancelAnimation);
+
+    grayscaleEffect.loadConfig();
+
+    effects.stackingOrder.forEach(grayscaleEffect.restartAnimation);
+  },
 };
 
 grayscaleEffect.init();
